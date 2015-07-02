5ba9c11a5f8398c5385288aef4f4183aec70de98
Eric Brelsford <ebrelsford@gmail.com>
2015-07-02 00:41:43 -0400

    Use no-edit

--- build/docs.sh
+++ build/docs.sh
@@ -8,7 +8,7 @@ npm run docs
 git add docs
 git commit -m "Latest documentation"
 git checkout $PUBLISH_BRANCH
-git pull --no-commit origin $PUBLISH_BRANCH
-git merge --no-commit $START_BRANCH
+git pull --no-edit origin $PUBLISH_BRANCH
+git merge --no-edit $START_BRANCH
 git push origin $PUBLISH_BRANCH
 git checkout $START_BRANCH
