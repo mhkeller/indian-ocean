cd45bab3557a759915b9203184577f6d5141575c
Eric Brelsford <ebrelsford@gmail.com>
2015-07-14 11:39:02 -0400

    Attempt to build docs automatically on travis

---
+++ build/travis-docs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Build docs on Travis and publish to gh-pages
+
+# Only run if on master, not a pull request, and for one node version (so we
+# don't end up running this once for each version).
+if [ "$TRAVIS_REPO_SLUG" == "mhkeller/indian-ocean" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_NODE_VERSION" == "0.12"]; then
+
+  echo "Publishing docs...\n"
+
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+
+  ./build/docs.sh
+
+  echo "Published docs to gh-pages.\n"
+
+fi
