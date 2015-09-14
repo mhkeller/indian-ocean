e35cd3334102f7190635af9264a171adb798e619
mhkeller <michael.keller@gmail.com>
2015-09-14 11:28:00 +0900

    add text readers and tests for them

--- test/test.js
+++ test/test.js
@@ -120,6 +120,21 @@ describe('readTsvSync()', function () {
   })
 })
 
+describe('readTxtSync()', function () {
+  describe('empty', function () {
+    it('should be empty', function () {
+      assert.lengthOf(io.readTxtSync(testDataPath('txt/empty.txt')), 0)
+    })
+  })
+
+  describe('basic', function () {
+    it('should match expected json', function () {
+      var txt = io.readTxtSync(testDataPath('txt/basic.txt'))
+      assert(_.isEqual(txt, 'The carbon in our apple pies billions upon billions cosmos. Extraplanetary Hypatia. Tendrils of gossamer clouds? Rogue stirred by starlight across the centuries cosmic ocean white dwarf billions upon billions the carbon in our apple pies Tunguska event, kindling the energy hidden in matter a still more glorious dawn awaits birth how far away quasar, vastness is bearable only through love of brilliant syntheses light years cosmic fugue, the carbon in our apple pies, astonishment hearts of the stars from which we spring inconspicuous motes of rock and gas realm of the galaxies how far away decipherment radio telescope a mote of dust suspended in a sunbeam gathered by gravity a very small stage in a vast cosmic arena a mote of dust suspended in a sunbeam.'))
+    })
+  })
+})
+
 describe('readdirInclude()', function () {
   describe('empty', function () {
     it('should be empty', function (done) {
