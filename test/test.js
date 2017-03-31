8796820bd7a472f2561c9fff425c38120046201e
mhkeller <code@mhkeller.com>
2017-03-31 13:44:00 +0200

    add direct parser methods

--- test/test.js
+++ test/test.js
@@ -139,11 +139,19 @@ describe('discernParser()', function () {
     it('should be text parser', function () {
       assert.equal(io.discernParser('/fake/path/what_is_this_file').toString(), io.parsers.txt.toString())
     })
+
+    it('should be text parser as method', function () {
+      assert.equal(io.discernParser('/fake/path/what_is_this_file').toString(), io.parseTxt.toString())
+    })
   })
 
   describe('csv', function () {
     it('should be csv parser', function () {
       assert.equal(io.discernParser(testDataPath('csv/empty.csv')).toString(), io.parsers.csv.toString())
+    })
+
+    it('should be csv parser as method', function () {
+      assert.equal(io.discernParser(testDataPath('csv/empty.csv')).toString(), io.parseCsv.toString())
     })
   })
 
@@ -151,11 +159,19 @@ describe('discernParser()', function () {
     it('should be tsv parser', function () {
       assert.equal(io.discernParser(testDataPath('tsv/empty.tsv')).toString(), io.parsers.tsv.toString())
     })
+
+    it('should be tsv parser as method', function () {
+      assert.equal(io.discernParser(testDataPath('tsv/empty.tsv')).toString(), io.parseTsv.toString())
+    })
   })
 
   describe('psv', function () {
     it('should be psv parser', function () {
       assert.equal(io.discernParser(testDataPath('psv/empty.psv')).toString(), io.parsers.psv.toString())
+    })
+
+    it('should be psv parser as method', function () {
+      assert.equal(io.discernParser(testDataPath('psv/empty.psv')).toString(), io.parsePsv.toString())
     })
   })
 
@@ -163,11 +179,19 @@ describe('discernParser()', function () {
     it('should be yaml parser', function () {
       assert.equal(io.discernParser(testDataPath('yaml/empty.yaml')).toString(), io.parsers.yaml.toString())
     })
+
+    it('should be yaml parser as method', function () {
+      assert.equal(io.discernParser(testDataPath('yaml/empty.yaml')).toString(), io.parseYaml.toString())
+    })
   })
 
   describe('yml', function () {
     it('should be yml parser', function () {
       assert.equal(io.discernParser(testDataPath('yml/empty.yml')).toString(), io.parsers.yml.toString())
+    })
+
+    it('should be yml parser as method', function () {
+      assert.equal(io.discernParser(testDataPath('yml/empty.yml')).toString(), io.parseYaml.toString())
     })
   })
 
@@ -175,11 +199,19 @@ describe('discernParser()', function () {
     it('should be txt parser', function () {
       assert.equal(io.discernParser(testDataPath('txt/empty.txt')).toString(), io.parsers.txt.toString())
     })
+
+    it('should be txt parser as method', function () {
+      assert.equal(io.discernParser(testDataPath('txt/empty.txt')).toString(), io.parseTxt.toString())
+    })
   })
 
   describe('aml', function () {
     it('should be aml parser', function () {
       assert.equal(io.discernParser(testDataPath('aml/empty.aml')).toString(), io.parsers.aml.toString())
+    })
+
+    it('should be aml parser as method', function () {
+      assert.equal(io.discernParser(testDataPath('aml/empty.aml')).toString(), io.parseAml.toString())
     })
   })
 
@@ -187,17 +219,29 @@ describe('discernParser()', function () {
     it('should be json parser', function () {
       assert.equal(io.discernParser(testDataPath('json/empty.json')).toString(), io.parsers.json.toString())
     })
+
+    it('should be json parser as method', function () {
+      assert.equal(io.discernParser(testDataPath('json/empty.json')).toString(), io.parseJson.toString())
+    })
   })
 
   describe('geojson', function () {
     it('should be geojson parser', function () {
       assert.equal(io.discernParser(testDataPath('geojson/empty.geojson')).toString(), io.parsers.geojson.toString())
     })
+
+    it('should be geojson parser as method', function () {
+      assert.equal(io.discernParser(testDataPath('geojson/empty.geojson')).toString(), io.parseJson.toString())
+    })
   })
 
   describe('topojson', function () {
     it('should be topojson parser', function () {
       assert.equal(io.discernParser(testDataPath('topojson/empty.topojson')).toString(), io.parsers.topojson.toString())
+    })
+
+    it('should be topojson parser as method', function () {
+      assert.equal(io.discernParser(testDataPath('topojson/empty.topojson')).toString(), io.parseJson.toString())
     })
   })
 
