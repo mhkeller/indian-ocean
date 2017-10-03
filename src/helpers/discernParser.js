2c4ec09d44295412bda3ff9803bdc9db410bf1e7
mhkeller <code@mhkeller.com>
2017-10-03 19:56:04 -0400

    update docs for discernParser

--- src/helpers/discernParser.js
+++ src/helpers/discernParser.js
@@ -3,18 +3,18 @@ import discernFormat from './discernFormat'
 import parsers from '../parsers/index'
 
 /**
- * Given a `filePath` return a parser that can read that file as json. Parses as text if format not supported by a built-in parser. If given a delimter string as the second argument, return a parser for that delimiter regardless of `filePath`. Used internally by {@link readData} and {@link readDataSync}.
+ * Given a `filePath` return a parser that can read that file as json. Parses as text if format not supported by a built-in parser. If given a delimiter string as the second argument, return a parser for that delimiter regardless of `filePath`. Used internally by {@link readData} and {@link readDataSync}.
  *
  * @function discernParser
  * @param {String} [filePath] Input file path
  * @param {String} [delimiter] Alternative usage is to pass a delimiter string. Delegates to `dsv.dsvFormat`.
- * @returns {Function} A parser that can read the file
+ * @returns {Function} A parser that can parse a file string into json
  *
  * @example
  * var parser = io.discernParser('path/to/data.csv')
- * var json = parser('path/to/data.csv')
+ * var json = parser('name,price\nApple,120\nPear,300')
  * var parser = io.discernParser(null, '_')
- * var json = parser('path/to/data.usv')
+ * var json = parser('name_price\nApple_120\nPear_300')
  */
 export default function discernParser (filePath, delimiter) {
   if (delimiter) {
