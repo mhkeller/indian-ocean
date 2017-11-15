5ebf2f8a81b6d1515c01b82e27d35a6d0db42609
Julian Burgess <jburgess29@bloomberg.net>
2017-11-15 18:21:27 +0000

    Modules

--- rollup.config.node.js
+++ rollup.config.node.js
@@ -3,13 +3,43 @@ import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import sourcemaps from 'rollup-plugin-sourcemaps'
 
-export default {
-  input: 'index.node.js',
-  output: {
-    format: 'cjs',
-    file: 'dist/indian-ocean.node.js',
-    sourcemap: true
+export default [
+  {
+    input: 'index.node.js',
+    output: {
+      format: 'es',
+      file: 'dist/indian-ocean.es.js',
+      sourcemap: true
+    },
+    plugins: [ nodeResolve(), commonjs(), babel(), sourcemaps() ],
+    external: [
+      'fs',
+      'util',
+      'shapefile',
+      'archieml',
+      'd3-dsv',
+      'd3-queue',
+      'dbf',
+      'js-yaml',
+      'mkdirp',
+      'rimraf',
+      'shapefile',
+      'underscore'
+    ]
   },
-  plugins: [ nodeResolve(), commonjs(), babel(), sourcemaps() ],
-  external: ['fs', 'path', 'util', 'shapefile']
-}
+  {
+    input: 'index.node.js',
+    output: {
+      format: 'cjs',
+      file: 'dist/indian-ocean.cjs.js',
+      sourcemap: true
+    },
+    plugins: [ nodeResolve(), commonjs(), babel(), sourcemaps() ],
+    external: [
+      'fs',
+      'util',
+      'path',
+      'shapefile'
+    ]
+  }
+]
