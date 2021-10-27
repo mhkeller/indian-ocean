198f190e89e33049823c9f433e1800dc6fa44477
mhkeller <code@mhkeller.com>
2021-10-26 22:43:51 -0400

    fix indentation

--- index.node.js
+++ index.node.js
@@ -1,56 +1,56 @@
 // converters
-export {default as convertData} from './src/converters/convertData'
-export {default as convertDbfToData} from './src/converters/convertDbfToData'
-export {default as writeDbfToData} from './src/converters/convertDbfToData' // Legacy support
+export { default as convertData } from './src/converters/convertData';
+export { default as convertDbfToData } from './src/converters/convertDbfToData';
+export { default as writeDbfToData } from './src/converters/convertDbfToData'; // Legacy support
 // formatters
-export {default as formatters} from './src/formatters/index'
-export {default as formatCsv} from './src/formatters/csv'
-export {default as formatDbf} from './src/formatters/dbf'
-export {default as formatJson} from './src/formatters/json'
-export {default as formatPsv} from './src/formatters/psv'
-export {default as formatTsv} from './src/formatters/tsv'
-export {default as formatTxt} from './src/formatters/txt'
+export { default as formatters } from './src/formatters/index';
+export { default as formatCsv } from './src/formatters/csv';
+export { default as formatDbf } from './src/formatters/dbf';
+export { default as formatJson } from './src/formatters/json';
+export { default as formatPsv } from './src/formatters/psv';
+export { default as formatTsv } from './src/formatters/tsv';
+export { default as formatTxt } from './src/formatters/txt';
 // helpers
-export {default as discernFileFormatter} from './src/helpers/discernFileFormatter'
-export {default as discernFormat} from './src/helpers/discernFormat'
-export {default as discernParser} from './src/helpers/discernParser'
-export {default as exists} from './src/helpers/exists'
-export {default as existsSync} from './src/helpers/existsSync'
-export {default as extMatchesStr} from './src/helpers/extMatchesStr'
-export {default as getParser} from './src/helpers/getParser'
-export {default as makeDirectories} from './src/helpers/makeDirectories'
-export {default as makeDirectoriesSync} from './src/helpers/makeDirectoriesSync'
-export {default as matches} from './src/helpers/matches'
-export {default as matchesRegExp} from './src/helpers/matchesRegExp'
+export { default as discernFileFormatter } from './src/helpers/discernFileFormatter';
+export { default as discernFormat } from './src/helpers/discernFormat';
+export { default as discernParser } from './src/helpers/discernParser';
+export { default as exists } from './src/helpers/exists';
+export { default as existsSync } from './src/helpers/existsSync';
+export { default as extMatchesStr } from './src/helpers/extMatchesStr';
+export { default as getParser } from './src/helpers/getParser';
+export { default as makeDirectories } from './src/helpers/makeDirectories';
+export { default as makeDirectoriesSync } from './src/helpers/makeDirectoriesSync';
+export { default as matches } from './src/helpers/matches';
+export { default as matchesRegExp } from './src/helpers/matchesRegExp';
 // parsers
-export {default as parsers} from './src/parsers/index'
-export {default as parseAml} from './src/parsers/aml'
-export {default as parseCsv} from './src/parsers/csv'
-export {default as parseJson} from './src/parsers/json'
-export {default as parsePsv} from './src/parsers/psv'
-export {default as parseTsv} from './src/parsers/tsv'
-export {default as parseTxt} from './src/parsers/txt'
+export { default as parsers } from './src/parsers/index';
+export { default as parseAml } from './src/parsers/aml';
+export { default as parseCsv } from './src/parsers/csv';
+export { default as parseJson } from './src/parsers/json';
+export { default as parsePsv } from './src/parsers/psv';
+export { default as parseTsv } from './src/parsers/tsv';
+export { default as parseTxt } from './src/parsers/txt';
 // readers
-export {default as readData} from './src/readers/readData'
-export {default as readDataSync} from './src/readers/readDataSync'
-export {default as readdirFilter} from './src/readers/readdirFilter'
-export {default as readdirFilterSync} from './src/readers/readdirFilterSync'
+export { default as readData } from './src/readers/readData';
+export { default as readDataSync } from './src/readers/readDataSync';
+export { default as readdirFilter } from './src/readers/readdirFilter';
+export { default as readdirFilterSync } from './src/readers/readdirFilterSync';
 // directReaders
-export {default as readAml} from './src/directReaders/readAml'
-export {default as readAmlSync} from './src/directReaders/readAmlSync'
-export {default as readCsv} from './src/directReaders/readCsv'
-export {default as readCsvSync} from './src/directReaders/readCsvSync'
-export {default as readDbf} from './src/directReaders/readDbf'
-export {default as readJson} from './src/directReaders/readJson'
-export {default as readJsonSync} from './src/directReaders/readJsonSync'
-export {default as readPsv} from './src/directReaders/readPsv'
-export {default as readPsvSync} from './src/directReaders/readPsvSync'
-export {default as readTsv} from './src/directReaders/readTsv'
-export {default as readTsvSync} from './src/directReaders/readTsvSync'
-export {default as readTxt} from './src/directReaders/readTxt'
-export {default as readTxtSync} from './src/directReaders/readTxtSync'
+export { default as readAml } from './src/directReaders/readAml';
+export { default as readAmlSync } from './src/directReaders/readAmlSync';
+export { default as readCsv } from './src/directReaders/readCsv';
+export { default as readCsvSync } from './src/directReaders/readCsvSync';
+export { default as readDbf } from './src/directReaders/readDbf';
+export { default as readJson } from './src/directReaders/readJson';
+export { default as readJsonSync } from './src/directReaders/readJsonSync';
+export { default as readPsv } from './src/directReaders/readPsv';
+export { default as readPsvSync } from './src/directReaders/readPsvSync';
+export { default as readTsv } from './src/directReaders/readTsv';
+export { default as readTsvSync } from './src/directReaders/readTsvSync';
+export { default as readTxt } from './src/directReaders/readTxt';
+export { default as readTxtSync } from './src/directReaders/readTxtSync';
 // writers
-export {default as appendData} from './src/writers/appendData'
-export {default as appendDataSync} from './src/writers/appendDataSync'
-export {default as writeData} from './src/writers/writeData'
-export {default as writeDataSync} from './src/writers/writeDataSync'
+export { default as appendData } from './src/writers/appendData';
+export { default as appendDataSync } from './src/writers/appendDataSync';
+export { default as writeData } from './src/writers/writeData';
+export { default as writeDataSync } from './src/writers/writeDataSync';
